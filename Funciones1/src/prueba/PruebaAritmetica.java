58986c1ee7c1ae2e65d65353093d0e4b0349a163
Derk4949 <xcal4949@hotmail.com>
2026-07-05 22:42:50 -0500

    RETO HECHO

--- Funciones1/src/prueba/PruebaAritmetica.java
+++ Funciones1/src/prueba/PruebaAritmetica.java
@@ -6,6 +6,9 @@ public class PruebaAritmetica {
     public static void main(String[] args) {
         System.out.println("*** Ejemplo Aritmetica ***");
         Aritmetica aritmetica1 = new Aritmetica(5, 7);
+        System.out.println("atributo operando1: "+ aritmetica1.getOperando1());
+        aritmetica1.setOperando1(10);
+        aritmetica1.setOperando2(15);
         aritmetica1.sumar();
         aritmetica1.restar();
         // Creamos un segundo objeto
